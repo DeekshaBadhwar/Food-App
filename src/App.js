21236a3e2b16a24babc5d1eeca66bfe4b2ac84c8
DeekshaBadhwar <deeksha.badhwar@netsmartz.net>
2022-02-25 17:35:36 +0530

    step1 completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home'
+import Drop from './components/dropdown';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Drop/>
     </div>
   );
 }
